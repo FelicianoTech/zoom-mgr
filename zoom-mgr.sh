a8937e7407defac4d573caa58500fc65fe94086c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-08-22 18:48:44 -0400

    Add missing pkg for Disco.

--- zoom-mgr.sh
+++ zoom-mgr.sh
@@ -11,7 +11,7 @@ install_zoom(){
 			pkgs="libgl1-mesa-glx libxcb-xtest0"
 			;;
 		disco)
-			pkgs="libxcb-xtest0"
+			pkgs="libgl1-mesa-glx libxcb-xtest0"
 			;;
 		*)
 			echo "Your operating system is unsupported."
